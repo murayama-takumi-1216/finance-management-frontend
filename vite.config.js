6c03f709f2e935acd702834994b73e7e73f24a8d
murayama-takumi-1216 <murayama.takumi.20021216@gmail.com>
2025-12-01 16:11:13 +0100

    fix backend url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        // target: 'http://localhost:3000',
+        target: 'https://finance-management-express-backend-production.up.railway.app';
         changeOrigin: true,
       }
     }
